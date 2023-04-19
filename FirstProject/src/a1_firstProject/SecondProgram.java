7223fe9fae88e22b794265be47b6fbe28c46ccff
jmsalexmech054 <jmsalexmech054@gmail.com>
2023-04-19 11:29:59 +0530

    neww one added

---
+++ FirstProject/src/a1_firstProject/SecondProgram.java
@@ -0,0 +1,14 @@
+package a1_firstProject;
+
+public class SecondProgram {
+
+	 public static void main(String[] args) {
+	        int a=9;
+	        if(a%2==0){
+	            System.out.println("The number is even: "+a);
+
+	        }else {
+	            System.out.println("The number is odd: "+a);
+	        }
+	    }
+}
